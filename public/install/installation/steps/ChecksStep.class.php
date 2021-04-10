6ed6dd9d364cb9cdd091c761bc8df049c185e435
Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
2021-04-10 22:56:44 +0000

    Fixes old mysql module…

--- public/install/installation/steps/ChecksStep.class.php
+++ public/install/installation/steps/ChecksStep.class.php
@@ -9,21 +9,21 @@
   * @http://www.projectpier.org/
   */
   class ChecksStep extends ScriptInstallerStep {
-    
+
     /**
     * Array of files and folders that need to writable
     *
     * @var array
     */
     private $check_is_writable = null;
-    
+
     /**
     * Array of extensions that need to be present for ProjectPier to be installed
     *
     * @var array
     */
     private $check_extensions = null;
-  
+
     /**
     * Construct the ChecksStep
     *
@@ -33,7 +33,7 @@
     */
     function __construct() {
       $this->setName('Server check');
-      
+
       $this->check_is_writable = array(
         '/config',
         '/public/files',
@@ -41,19 +41,19 @@
         '/cache',
         '/upload'
       ); // array
-      
+
       $this->check_extensions = array(
-        'session' => true, 
-        'mysql' => true, 
-        'calendar' => false, 
-        'gd' => false, 
+        'session' => true,
+        'mysqli' => true,      // now we need to check for mysqli, not mysql which is deprecated (gwyneth 20210410)
+        'calendar' => false,
+        'gd' => false,
         'simplexml' => false,
         'ldap' => false,
-        'sockets' => false, 
+        'sockets' => false,
       ); // array
 
     } // __construct
-    
+
     /**
     * Execute environment checks
     *
@@ -63,15 +63,15 @@
     */
     function execute() {
       $all_ok = true;
-      
+
       // Check PHP version
-      if (version_compare(PHP_VERSION, '5.0.2', 'ge')) {
+      if (version_compare(PHP_VERSION, '5.6', 'ge')) {  // bumping version
         $this->addToChecklist('PHP version is ' . PHP_VERSION, true);
       } else {
         $this->addToChecklist('Error: PHP version on this system is ' . PHP_VERSION . '. PHP 5.0.2 or newer is required', false);
         $all_ok = false;
       } // if
-      
+
       foreach ($this->check_extensions as $extension_name => $required) {
         if (extension_loaded($extension_name)) {
           $this->addToChecklist("'$extension_name' extension is available", true);
@@ -84,7 +84,7 @@
           }
         } // if
       } // if
-      
+
       if (is_array($this->check_is_writable)) {
         foreach ($this->check_is_writable as $relative_folder_path) {
           $check_this = INSTALLATION_PATH . $relative_folder_path;
@@ -94,14 +94,14 @@
             $all_ok = false;
             continue;
           }
-          
+
           $is_writable = false;
           if (is_file($check_this)) {
             $is_writable = file_is_writable($check_this);
           } elseif (is_dir($check_this)) {
             $is_writable = folder_is_writable($check_this);
           } // if
-          
+
           if ($is_writable) {
             $this->addToChecklist("$relative_folder_path is writable", true);
           } else {
@@ -110,21 +110,21 @@
           } // if
         } // foreach
       } // if
-      
+
       $this->setContentFromTemplate('checks.php');
-      
+
       if (ini_get('zend.ze1_compatibility_mode')) {
         $this->addToChecklist('zend.ze1_compatibility_mode is set to On. This can cause some strange problems. It is strongly suggested to turn this value to Off (in your php.ini file)', false);
       } // if
-      
+
       if ($all_ok) {
         return $this->isSubmitted();
       } // if
-      
+
       $this->setNextDisabled(true);
       return false;
     } // execute
-  
+
   } // ChecksStep
 
 ?>
--- public/install/installation/steps/SystemConfigStep.class.php
+++ public/install/installation/steps/SystemConfigStep.class.php
@@ -1,8 +1,8 @@
 <?php
 
   /**
-  * Third step of ProjectPier installation -> user is required to enter 
-  * database connection settings. Script tries the connection params and 
+  * Third step of ProjectPier installation -> user is required to enter
+  * database connection settings. Script tries the connection params and
   * if correct saves them in /config/config.php file
   *
   * @package ScriptInstaller
@@ -11,14 +11,14 @@
   * @http://www.projectpier.org/
   */
   class SystemConfigStep extends ScriptInstallerStep {
-    
+
     /**
     * Cached database connection resource
     *
     * @var resource
     */
     private $database_connection;
-  
+
     /**
     * Construct the ConfigStep
     *
@@ -29,7 +29,7 @@
     function __construct() {
       $this->setName('Database configuration');
     } // __construct
-    
+
     /**
     * Prepare and process config form
     *
@@ -42,14 +42,14 @@
       } else {
         $protocol = 'http://';
       } // if
-      
+
       $request_url = without_slash($protocol . dirname($_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME']));
       if (($rpos = strrpos($request_url, '/')) !== false) {
         $installation_url = substr($request_url, 0, $rpos - 7); // remove /public ;)
       } else {
         $installation_url = '';
       } // if
-      
+
       $config_form_data = array_var($_POST, 'config_form');
       if (!is_array($config_form_data)) {
         $config_form_data = array(
@@ -64,7 +64,7 @@
         ); // array
       } // if
       tpl_assign('config_form_data', $config_form_data);
-      
+
       if ($this->isSubmitted()) {
         $database_type    = (string) array_var($config_form_data, 'database_type');
         $database_host    = (string) array_var($config_form_data, 'database_host');
@@ -74,12 +74,12 @@
         $database_create  = (string) array_var($config_form_data, 'database_create');
         $database_charset = (string) array_var($config_form_data, 'database_charset');
         $database_prefix  = (string) array_var($config_form_data, 'database_prefix');
-        
+
         if ($this->database_connection = @mysql_connect($database_host, $database_user, $database_pass)) {
           // ---------------------------------------------------
           //  Check if we have at least 4.1
           // ---------------------------------------------------
-          $mysql_version = mysql_get_server_info($this->database_connection);
+          $mysql_version = mysqli_get_server_info($this->database_connection);
           if ($mysql_version && version_compare($mysql_version, '4.1', '>=')) {
             $this->addToStorage('database_type', $database_type);
             $this->addToStorage('database_host', $database_host);
@@ -88,26 +88,26 @@
             $try_select = true;
             if ($database_create=='yes') {
               $try_select = false;
-              if (@mysql_select_db(array_var($config_form_data, 'database_name'), $this->database_connection)) {
+              if (mysqli_select_db($this->database_connection, array_var($config_form_data, 'database_name'))) {
                   $this->addError("Database create failed, $database_name exists. Choose another name or uncheck create option.");
               } else {
                 $sql = "CREATE DATABASE `$database_name`";
-                if (mysql_query($sql, $this->database_connection)) {
+                if (mysqli_query($this->database_connection, $sql)) {
                   $try_select = true;
                 } else {
-                  $this->addError('Error creating database: ' . mysql_error() );
+                  $this->addError('Error creating database: ' . mysqli_error($this->database_connection) );
                 }
               }
             }
             if ($try_select) {
-              if (@mysql_select_db(array_var($config_form_data, 'database_name'), $this->database_connection)) {
+              if (mysqli_select_db($this->database_connection, array_var($config_form_data, 'database_name'))) {
                 $this->addToStorage('database_name', $database_name);
                 $this->addToStorage('database_charset', $database_charset);
                 $this->addToStorage('database_prefix', $database_prefix);
 
                 $exist = 0;
-                $result = @mysql_query("show tables like '$database_prefix%'", $this->database_connection);
-                while ($row = @mysql_fetch_row($result)) {
+                $result = mysqli_query($this->database_connection, "show tables like '$database_prefix%'");
+                while ($row = mysqli_fetch_row($result)) {
                   $exist++;
                 }
                 if ($exist==0) {
@@ -125,11 +125,11 @@
           $this->addError("Failed to connect to database server '$database_host' with user '$database_user' and provided password");
         } // if
       } // if
-      
+
       $this->setContentFromTemplate('system_config_form.php');
       return false;
     } // execute
-    
+
     /**
     * Add error message to all messages and break the execution
     *
@@ -140,12 +140,12 @@
     function breakExecution($error_message) {
       $this->addToChecklist($error_message, false);
       if (is_resource($this->database_connection)) {
-        @mysql_query('ROLLBACK', $this->database_connection);
+        mysqli_query($this->database_connection, 'ROLLBACK');
       }
       $this->setContentFromTemplate('finish.php');
       return false;
     } // breakExecution
-    
+
     /**
     * Write $constants in config file
     *
@@ -157,13 +157,13 @@
       tpl_assign('config_file_constants', $constants);
       return file_put_contents(INSTALLATION_PATH . '/config/config.php', tpl_fetch(get_template_path('config_file.php')));
     } // writeConfigFile
-    
+
     /**
     * Execute multiple queries
-    * 
+    *
     * This one is really quick and dirty because I want to finish this and catch
     * the bus. Need to be redone ASAP
-    * 
+    *
     * This function returns true if all queries are executed successfully
     *
     * @access public
@@ -179,31 +179,31 @@
         $executed_queries = 0;
         return true;
       } // if
-      
+
       // Make it work on PHP 5.0.4
       $sql = str_replace(array("\r\n", "\r"), array("\n", "\n"), $sql);
-      
+
       $queries = explode(";\n", $sql);
       if (!is_array($queries) || !count($queries)) {
         $total_queries = 0;
         $executed_queries = 0;
         return true;
       } // if
-      
+
       $total_queries = count($queries);
       foreach ($queries as $query) {
         if (trim($query)) {
-          if (@mysql_query(trim($query))) {
+          if (mysqli_query($this->database_connection, trim($query))) {
             $executed_queries++;
           } else {
             return false;
           } // if
         } // if
       } // if
-      
+
       return true;
     } // executeMultipleQueries
-  
+
   } // ConfigStep
 
 ?>
