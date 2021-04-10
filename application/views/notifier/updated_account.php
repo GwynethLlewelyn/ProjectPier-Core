a3a89c14583e965021c3de4319d35f4811a657be
Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
2021-04-10 18:36:32 +0000

    Are these part of the code?

--- application/views/notifier/updated_account.php
+++ application/views/notifier/updated_account.php
@@ -1,18 +1,25 @@
-<?php echo lang('hi john doe', $updated_account->getContact()->getDisplayName()) ?>,
+<!-- Should this be part of the code? (gwyneth 20210410) -->
 
+<?php echo lang("hi john doe", $updated_account->getContact()->getDisplayName()); ?>,
 
-<?php echo lang('user updated your account', $updated_account->getUpdatedBy()->getContact()->getDisplayName()) ?>. 
-<?php echo lang('visit and login', externalUrl(ROOT_URL)) ?>:
+<?php echo lang(
+  "user updated your account",
+  $updated_account
+    ->getUpdatedBy()
+    ->getContact()
+    ->getDisplayName()
+); ?>.
+<?php echo lang("visit and login", externalUrl(ROOT_URL)); ?>:
 
+<?php echo lang("username"); ?>: <?php echo $updated_account->getUsername(); ?>
 
-<?php echo lang('username') ?>: <?php echo $updated_account->getUsername() ?>
-
-<?php if (trim($raw_password) != '') { ?>
-<?php echo lang('password') ?>: <?php echo $raw_password ?>
+<?php if (trim($raw_password) != "") { ?>
+<?php echo lang("password"); ?>: <?php echo $raw_password; ?>
 <?php } else { ?>
-<?php echo lang('password unchanged') ?>
-<?php } // if?>
-
+<?php echo lang("password unchanged"); ?>
+<?php }
+// if
+?>
 
 --
-<?php echo externalUrl(ROOT_URL) ?>
+<?php echo externalUrl(ROOT_URL); ?>
