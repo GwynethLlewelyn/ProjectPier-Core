95f651ca23e9cdd916ad0ce7f64e5c92b235b14d
Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
2021-04-11 12:09:33 +0000

    :lipstick: Another bump to PHP 5.6

--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php
 
-  if (!version_compare(phpversion(), '5.0', '>=')) {
-    die('<strong>Installation error:</strong> in order to run ProjectPier you need PHP5. Your current PHP version is: ' . phpversion());
+  if (!version_compare(phpversion(), '5.6', '>=')) {
+    die('<strong>Installation error:</strong> in order to run ProjectPier you need PHP 5.6. Your current PHP version is: ' . phpversion());
   } // if
   if (!defined('PUBLIC_FOLDER')) {
     define('PUBLIC_FOLDER', '');
   } // if
-  require '../index.php'; 
-  
+  require '../index.php';
+
 ?>
