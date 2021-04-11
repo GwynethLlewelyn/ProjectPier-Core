866e6ceb57948241f9db60f2db537c6b6ef7216d
Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
2021-04-11 12:39:24 +0000

    :lipstick: Although “MySQL” is displayed, we’re pushing the select value to be “mysqli” or else everything will blow up :bomb:

--- public/install/installation/templates/system_config_form.php
+++ public/install/installation/templates/system_config_form.php
@@ -3,12 +3,12 @@
   <tr>
     <th colspan="2"></th>
   </tr>
-  
+
   <tr>
     <td class="optionLabel"><label for="configFormDatabaseType">Database type:</label></td>
     <td>
       <select name="config_form[database_type]" id="configFormDatabaseType">
-        <option value="mysql">MySQL</option>
+        <option value="mysqli">MySQL</option>
       </select>
     </td>
   </tr>
@@ -18,7 +18,7 @@
   <tr>
     <th colspan="2">Connection</th>
   </tr>
-  
+
   <tr>
     <td class="optionLabel"><label for="configFormDatabaseHost">Database host:</label></td>
     <td><input type="text" name="config_form[database_host]" id="configFormDatabaseHost" value="<?php echo array_var($config_form_data, 'database_host') ?>" /></td>
@@ -28,19 +28,19 @@
     <td class="optionLabel"><label for="configFormDatabaseUser">Database user:</label></td>
     <td><input type="text" name="config_form[database_user]" id="configFormDatabaseUser" value="<?php echo array_var($config_form_data, 'database_user') ?>" /></td>
   </tr>
-  
+
   <tr>
     <td class="optionLabel"><label for="configFormDatabasePass">Database password:</label></td>
     <td><input type="password" name="config_form[database_pass]" id="configFormDatabasePass" value="<?php echo array_var($config_form_data, 'database_pass') ?>" /></td>
   </tr>
-  
+
 </table>
 
 <table class="formBlock">
   <tr>
     <th colspan="2">Database</th>
   </tr>
-  
+
   <tr>
     <td class="optionLabel"><label for="configFormDatabaseName">Database name:</label></td>
     <td><input type="text" name="config_form[database_name]" id="configFormDatabaseName" value="<?php echo array_var($config_form_data, 'database_name') ?>" /></td>
@@ -60,7 +60,7 @@
     <td class="optionLabel"><label for="configFormDatabaseCharSet">Character set:</label></td>
     <td><input type="text" name="config_form[database_charset]" id="configFormDatabaseCharSet" readonly value="<?php echo array_var($config_form_data, 'database_charset') ?>" /></td>
   </tr>
-  
+
   <tr>
     <td class="optionLabel"><label for="configFormDatabasePrefix">Table prefix:</label></td>
     <td><input type="text" name="config_form[database_prefix]" id="configFormDatabasePrefix" value="<?php echo array_var($config_form_data, 'database_prefix') ?>" /></td>
